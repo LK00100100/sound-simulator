4a8d755ed436cb86e2dd409a04b4c5751055e937
L. K <LK00100100@gmail.com>
2023-11-20 19:17:44 -0500

    init project works

---
+++ script.js
@@ -0,0 +1,76 @@
+console.log("loaded script");
+
+let isPlay = false;
+
+function onload() {
+    console.log("loaded");
+
+    initSoundBoxes();
+}
+
+function initSoundBoxes() {
+    const soundsDiv = document.getElementById("sounds");
+
+    for (let i = 0; i < 5; i++) {
+        const soundBox = createSoundBox();
+        soundsDiv.appendChild(soundBox);
+    }
+}
+
+function clickPlay() {
+    console.log("clicked play");
+    isPlay = true;
+}
+
+function clickStop() {
+    console.log("clicked stop");
+    isPlay = false;
+}
+
+function clickAddSound() {
+    createSoundBox();
+}
+
+function createSoundBox() {
+
+    const timerSeconds = getRandomInt(10) + 1;
+    const hitChance = getRandomInt(100) + 1;
+
+    const soundBox = document.createElement("div");
+    soundBox.classList.add("soundbox")
+    soundBox.innerHTML = "🔊 slack message";
+    soundBox.innerHTML += `<br>⏱️ timer: ${timerSeconds} seconds`;
+    soundBox.innerHTML += `<br>🎲 hit chance : ${hitChance}%`;
+
+    const audio = new Audio('/audio/slack-message.wav');
+
+    audio.onended = function () {
+        soundBox.classList.remove("playing");
+    };
+
+    setInterval(() => {
+        if (!isPlay)
+            return;
+
+        let diceRoll = getRandomInt(100);
+
+        if (diceRoll > hitChance)
+            return;
+
+        audio.play();
+        soundBox.classList.add("playing");
+
+    }, timerSeconds * 1000);
+
+    return soundBox;
+}
+
+/**
+ * from 0 (inclusive) to max (exclusive).
+ * If max is 2, then 0 or 1.
+ * @param {number} max 
+ * @returns 
+ */
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
